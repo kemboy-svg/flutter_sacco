c8216580b25ee74f8ab4cf42df3e37c87217e785
mobisa-012 <mobisakwamboka@gmail.com>
2023-04-24 19:20:30 +0300

    changed the account screen UI to take a list vie

--- sacco_2/lib/account/account.dart
+++ sacco_2/lib/account/account.dart
@@ -1,38 +1,33 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:sacco_2/withdraw/menu.dart';
-
 
 class AccNumScreen extends StatefulWidget {
   const AccNumScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() => StartState();
+  State<AccNumScreen> createState() => _AccNumScreenState();
 }
 
-class StartState extends State<AccNumScreen> {
-  // FirebaseAuth auth = FirebaseAuth.instance;
-  // CollectionReference users = FirebaseFirestore.instance.collection('users');
-
+class _AccNumScreenState extends State<AccNumScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('MEMBER ACCOUNT'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Member Account'),
+        ),
+        body: SingleChildScrollView(
+            child: Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           margin: const EdgeInsets.only(left: 10, right: 10, top: 0),
           child: Column(
             children: [
               Image.asset(
-                "assets/15.png",
-                height: 200,
+                'assets/15.png',
                 width: 200,
+                height: 200,
               ),
               Text(
                 'MEMBER DETAILS',
@@ -44,127 +39,132 @@ class StartState extends State<AccNumScreen> {
                     ..color = Colors.blue,
                 ),
               ),
-              Container(
-                height: 250,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: StreamBuilder(
-                  stream: FirebaseFirestore.instance.collection('register_members').snapshots(),
-                  builder:(context, snapshot){
-                    if (!snapshot.hasData){
-                      return  const CircularProgressIndicator();
-                    }
-                    DocumentSnapshot<Map<String, dynamic>> data = snapshot.data!.docs[index];
-                  } 
-                     
-
-                      String balance = '${data['Balance']}';
-                      var doubleBalance = double.parse(balance);
-                      double bookBalance = doubleBalance - 500;
-                      String imageUrl = '${data['IDCardPath']}';
-                      return Column(
-                        children: [
-                          Text(
-                            'NAME: ${data['First_name']}',
+              StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('register_members')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+                  List<QueryDocumentSnapshot<Map<String, dynamic>>> data =
+                      snapshot.data!.docs;
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot data = snapshot.data!.docs[index];
+                      return  ListTile(
+                        leading: const CircleAvatar(
+                          backgroundColor: Colors.blue,
+                        ),
+                        title: Row(
+                          children: [
+                            Text('NAME: ${data['fname']}',
                             style: TextStyle(
-                              fontSize: 15,
-                              foreground: Paint()
-                                ..style = PaintingStyle.stroke
-                                ..strokeWidth = 0.5
-                                ..color = Colors.black,
-                            ),
+                                fontSize: 15,
+                                foreground: Paint()
+                                  ..style = PaintingStyle.stroke
+                                  ..strokeWidth = 0.5
+                                  ..color = Colors.black,
+                              ),),
+                              Text('ID NO: ${data['Last_name']}',
+                              style: TextStyle(
+                                fontSize: 15,
+                                foreground: Paint()
+                                  ..style = PaintingStyle.stroke
+                                  ..strokeWidth = 0.5
+                                  ..color = Colors.black,
+                              )),
+                          ],
+                        ),
+                          subtitle: Column(
+                            children: [                              
+                                Text(
+                          'BALANCE: ${data['Balance']}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 0.5
+                              ..color = Colors.black,
                           ),
-                          Text(
-                            'ID NO: ${data['Last_name']}',
-                            style: TextStyle(
-                              fontSize: 15,
-                              foreground: Paint()
-                                ..style = PaintingStyle.stroke
-                                ..strokeWidth = 0.5
-                                ..color = Colors.black,
-                            ),
+                        ),
+                              Text(
+                          'PHONE NO: ${data['phoneNumber']}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 0.5
+                              ..color = Colors.black,
                           ),
-                          Text(
-                            'PHONE NO: ${data['phoneNumber']}',
-                            style: TextStyle(
-                              fontSize: 15,
-                              foreground: Paint()
-                                ..style = PaintingStyle.stroke
-                                ..strokeWidth = 0.5
-                                ..color = Colors.black,
-                            ),
+                        ),
+                            ],
                           ),
-                          Text(
-                            'DOB: ${data['DOB']}',
-                            style: TextStyle(
-                              fontSize: 15,
-                              foreground: Paint()
-                                ..style = PaintingStyle.stroke
-                                ..strokeWidth = 0.5
-                                ..color = Colors.black,
-                            ),
+                          trailing:   Column(
+                            children: [
+                              Text(
+                          'DOB: ${data['DOB']}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 0.5
+                              ..color = Colors.black,
                           ),
-                          Text(
-                            'BALANCE: ${data['Balance']}',
-                            style: TextStyle(
-                              fontSize: 15,
-                              foreground: Paint()
-                                ..style = PaintingStyle.stroke
-                                ..strokeWidth = 0.5
-                                ..color = Colors.black,
-                            ),
+                        ),
+                              Text(
+                              'LOCATION: ${data['Location']}',
+                              style: TextStyle(
+                                fontSize: 15,
+                                foreground: Paint()
+                                  ..style = PaintingStyle.stroke
+                                  ..strokeWidth = 0.5
+                                  ..color = Colors.black,
+                              ),
+                        ),
+                           Text(
+                          'DOB: ${data['DOB']}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 0.5
+                              ..color = Colors.black,
                           ),
-                          Text(
-                            'ADDRESS${bookBalance.toStringAsFixed(2)}',
-                            style: TextStyle(
-                              fontSize: 15,
-                              foreground: Paint()
-                                ..style = PaintingStyle.stroke
-                                ..strokeWidth = 0.5
-                                ..color = Colors.black,
-                            ),
+                        ),
+                        //  Text(
+                        //   'ADDRESS${bookBalance.toStringAsFixed(2)}',
+                        //   style: TextStyle(
+                        //     fontSize: 15,
+                        //     foreground: Paint()
+                        //       ..style = PaintingStyle.stroke
+                        //       ..strokeWidth = 0.5
+                        //       ..color = Colors.black,
+                        //   ),
+                        // ),
+                            ],
                           ),
-                          Text(
-                            'LOCATION: ${data['Location']}',
-                            style: TextStyle(
-                              fontSize: 15,
-                              foreground: Paint()
-                                ..style = PaintingStyle.stroke
-                                ..strokeWidth = 0.5
-                                ..color = Colors.black,
-                            ),
-                          ),
-                          Container(
-                            height: 99,
-                            width: 150,
-                            margin: const EdgeInsets.only(left: 0, right: 0, top: 20),
-                            padding: const EdgeInsets.only(left: 5, right: 5),
-                            child: Image.network(
-                              imageUrl,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ],
                       );
-                    
-                    return const Text('Loading');
-                  },
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => const menuScreen()));
-                  
+                    },
+                  );
                 },
-                child: const Text(
-                 "BACK TO MAIN MENU",
-                 style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+                    
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => const menuScreen()));
+                  },
+                  child: const Text(
+                    "BACK TO MAIN MENU",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
                   ),
+                ),
               ),
             ],
           ),
-        ),
-      ),
-    );
+        )));
   }
 }
